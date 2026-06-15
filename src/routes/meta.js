dffcd4d6eef80d747b4775a3760aa212d9235403
Apkapay <veerendra.d@apkapay.com>
2026-06-15 14:40:54 +0530

    Revert "Revert "Revert "Add social login to builder side"""
    This reverts commit e78c17bb27dbeb9c2e404bd3a177f9095d4228d7.

--- src/routes/meta.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { CATEGORIES, PTYPES, REWARDS, FILTERS } from "../meta.js";
-
-export const router = Router();
-
-router.get("/", (req, res) => {
-  res.json({ categories: CATEGORIES, ptypes: PTYPES, rewards: REWARDS, filters: FILTERS });
-});
--- src/vutil.js
+++
@@ -1,9 +0,0 @@
-export function deadlineHours(deadline) {
-  const m = /(\d+)\s*([hd])/.exec(deadline || "");
-  if (!m) return 999;
-  return m[2] === "d" ? +m[1] * 24 : +m[1];
-}
-
-export function deadlineLabel(deadline) {
-  return (deadline || "").replace("Closes ", "");
-}
